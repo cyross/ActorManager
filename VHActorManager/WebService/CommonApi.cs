8a49bf5f71e0083209dcd3c61d35fc4141a8bb2d
Cyross Makoto <takuan45189@gmail.com>
2023-03-28 17:49:00 +0900

    JSON保存時に、VEGASスクリプト・拡張にも拡げるときの処理を追加

--- VHActorManager/WebService/CommonApi.cs
+++ VHActorManager/WebService/CommonApi.cs
@@ -1,9 +1,4 @@
 ﻿using Grapevine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VHActorManager.Master;
 
 namespace VHActorManager.WebService
@@ -17,6 +12,26 @@ namespace VHActorManager.WebService
             ActorMaster.Instance().Save();
             VoiceEngineMaster.Instance().Save();
             ColorMaster.Instance().Save();
+
+            User user = User.Instance();
+
+            string scriptPath = user.VegasScriptYAML.ScriptPath;
+
+            if (user.VegasScriptYAML.Enable && Path.Exists(scriptPath))
+            {
+                ActorMaster.Instance().Save(Utility.CombineFilePath(scriptPath, ActorMaster.YAML_FILENAME));
+                VoiceEngineMaster.Instance().Save(Utility.CombineFilePath(scriptPath, VoiceEngineMaster.YAML_FILENAME));
+                ColorMaster.Instance().Save(Utility.CombineFilePath(scriptPath, ColorMaster.YAML_FILENAME));
+            }
+
+            string extPath = user.VegasScriptYAML.ExtPath;
+
+            if (user.VegasScriptYAML.Enable && Path.Exists(extPath))
+            {
+                ActorMaster.Instance().Save(Utility.CombineFilePath(extPath, ActorMaster.YAML_FILENAME));
+                VoiceEngineMaster.Instance().Save(Utility.CombineFilePath(extPath, VoiceEngineMaster.YAML_FILENAME));
+                ColorMaster.Instance().Save(Utility.CombineFilePath(extPath, ColorMaster.YAML_FILENAME));
+            }
 
             await ApiUtils.CreateAndSendResponse(context, new ResponseMessage().Succeed().ToJson());
         }
